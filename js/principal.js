9ff4c9b40874797f649cb8695b84b0d181db7170
gabisegger <gabi.segger@gmail.com>
2022-05-30 10:44:44 -0300

    feat: inserting new row in table with values ​​filled in form

--- js/principal.js
+++ js/principal.js
@@ -38,4 +38,43 @@ for(var i = 0; i < pacientes.length; i++) {
     }
 }
 
+var botaoAdicionar = document.querySelector("#adicionar-paciente");
+botaoAdicionar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    //Pegando os valores preenchidos no form
+    var form = document.querySelector("#form-adiciona");
+
+    var nome = form.nome.value;
+    var peso = form.peso.value;
+    var altura = form.altura.value;
+    var gordura = form.gordura.value;
+
+    //Criando mais uma linha que armazena os dados preenchidos no form
+    var pacienteTr = document.createElement("tr");
+
+    var nomeTd = document.createElement("td");
+    var pesoTd = document.createElement("td");
+    var alturaTd = document.createElement("td");
+    var gorduraTd = document.createElement("td");
+    var imcTd = document.createElement("td");
+
+    nomeTd.textContent = nome;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+ 
+    pacienteTr.appendChild(nomeTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    //Adicionando a nova linha com o preenchimento do form na tabela
+    var tabela = document.querySelector("#tabela-pacientes");
+
+    tabela.appendChild(pacienteTr);
+
+})
+
+
 
