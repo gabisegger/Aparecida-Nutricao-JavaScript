237508ae4ab6d518c9a0b74f7ba27b7e9547c3db
gabisegger <gabi.segger@gmail.com>
2022-06-01 17:02:44 -0300

    feat: creation of the functions validaAltura and validaPeso

--- js/calcula-imc.js
+++ js/calcula-imc.js
@@ -17,17 +17,17 @@ for(var i = 0; i < pacientes.length; i++) {
 
     var tdImc = paciente.querySelector(".info-imc");
 
-    var pesoValido = true;
-    var alturaValida = true;
+    var pesoValido = validaPeso(peso);
+    var alturaValida = validaAltura(altura);
 
-    if(peso <= 0 || peso >= 1000){
+    if(!pesoValido) {
         console.log("Peso inválido!");
         pesoValido = false;
         tdImc.textContent = "Peso inválido!";
         paciente.classList.add("paciente-invalido");
     }
 
-    if(altura <= 0 || altura >= 3.00){
+    if(!alturaValida){
         console.log("Altura inválida!");
         alturaValida = false;
         tdImc.textContent = "Altura inválida!";
@@ -37,6 +37,22 @@ for(var i = 0; i < pacientes.length; i++) {
     if( alturaValida && pesoValido){
         var imc = calculaIMC(peso,altura);
         tdImc.textContent = imc;
+    }
+}
+
+function validaPeso(peso){
+    if(peso >= 0 && peso < 1000){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+function validaAltura(altura){
+    if(altura >= 0 && altura <= 3.0){
+        return true;
+    }else{
+        return false;
     }
 }
 
