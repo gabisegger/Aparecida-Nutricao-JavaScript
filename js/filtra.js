7bc9887487b9e308d0a4f79492594596658a408a
gabisegger <gabi.segger@gmail.com>
2022-06-14 13:36:51 -0300

    feat: Filtering with regular expressions

--- js/filtra.js
+++ js/filtra.js
@@ -2,10 +2,25 @@ var campoFiltro = document.querySelector("#filtrar-tabela");
 
 campoFiltro.addEventListener("input", function(){
     console.log(this.value);
-    var pacientes = document.querySelectorAll(".pacientes");
-    for(var i = 0; i < pacientes.length; i++){
+    var pacientes = document.querySelectorAll(".paciente");
+
+    if(this.value.length > 0){
+        for(var i = 0; i < pacientes.length; i++){
+            var paciente = pacientes[i];
+            var tdNome = paciente.querySelector(".info-nome");
+            var nome = tdNome.textContent;
+            var expressao = new RegExp(this.value, "i");
+            if(!expressao.test(nome)){
+               paciente.classList.add("invisivel"); 
+            }else{
+                paciente.classList.remove("invisivel");
+            }
+        }
+    } else{
+       for (var i = 0; i < pacientes.length; i++){
         var paciente = pacientes[i];
-        var tdNome = paciente.querySelector(".info-nome");
-        var nome = paciente.textContent;
+        paciente.classList.remove("invisivel"); 
+       } 
     }
+    
 });
